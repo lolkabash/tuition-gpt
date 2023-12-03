eaab9f65fe552708a63723057ac1ce9e51e79ddb
Ray <lairay.lirui@gmail.com>
2023-12-03 18:26:35 +0800

    clean up

--- youtubetool.py
+++ youtubetool.py
@@ -10,8 +10,22 @@ load_dotenv()
 key=os.getenv("youtube_api_key")
 
 # Replace with your actual API key, channel IDs, and keywords
+
+channel_id_list = ['UCX6b17PVsYBQ0ip5gyeme-Q', 'UC_SvYP0k05UKiJ_2ndB02IA', 'UCEWpbFLzoYGPfuWUMFPSaoA']
+channel_name_list = ['CrashCourse', 'blackpenredpen', 'The Organic Chemistry Tutor']
+# keep_adding = True
+# while keep_adding:
+#     ans1=str(input('Input Username : ' )).split()
+#     channel_id,name=channelid(ans1)
+#     channel_id_list.append(channel_id)
+#     channel_name_list.append(name)
+#     resume = input("keep adding? y/n")
+#     if resume == "n":
+#         keep_adding = False
+resource_dictionary = dict(zip(channel_name_list,channel_id_list))
+print(resource_dictionary)
+
 API_KEY = os.getenv("youtube_api_key")
-CHANNEL_IDS = ['CHANNEL_ID_1', 'CHANNEL_ID_2']  # Replace with actual channel IDs
 channel_id_list = ['UCX6b17PVsYBQ0ip5gyeme-Q', 'UC_SvYP0k05UKiJ_2ndB02IA', 'UCEWpbFLzoYGPfuWUMFPSaoA']
 KEYWORDS = 'momentum'  
 
@@ -36,24 +50,6 @@ def returnurl(ucid):
 
 
 
-
-
-channel_id_list = ['UCX6b17PVsYBQ0ip5gyeme-Q', 'UC_SvYP0k05UKiJ_2ndB02IA', 'UCEWpbFLzoYGPfuWUMFPSaoA']
-channel_name_list = ['CrashCourse', 'blackpenredpen', 'The Organic Chemistry Tutor']
-keep_adding = True
-# while keep_adding:
-#     ans1=str(input('Input Username : ' )).split()
-#     channel_id,name=channelid(ans1)
-#     channel_id_list.append(channel_id)
-#     channel_name_list.append(name)
-#     resume = input("keep adding? y/n")
-#     if resume == "n":
-#         keep_adding = False
-
-
-resource_dictionary = dict(zip(channel_name_list,channel_id_list))
-print(resource_dictionary)
-
 import requests
 
 def search_videos(api_key, channel_ids, keywords):
@@ -67,7 +63,7 @@ def search_videos(api_key, channel_ids, keywords):
             'channelId': channel_id,
             'type': 'video',
             'q': keywords,
-            'maxResults': 50,  # You can adjust this number
+            'maxResults': 5,  # You can adjust this number
             'key': api_key
         }
         response = requests.get(base_url, params=params).json()
