796f32f1fdbbd8e9c4706396c3e854392577a2a6
Ray <lairay.lirui@gmail.com>
2023-12-03 17:37:10 +0800

    youtube search for channel function

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pytesseract
 from PIL import Image
 
 
-
 images = convert_from_path('olevel4048mathsyllabus.pdf')
 
 for i, image in enumerate(images):
     text = pytesseract.image_to_string(image)
     print(f"===== Page {i+1} =====")
     print(text)
+    
